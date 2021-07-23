7c9fa8fa86345d80698459f2252c571e9ee315db
Levi <shrieverdev@gmail.com>
2021-07-23 13:18:34 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Quiz-App",
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-react-helmet"],
+};
