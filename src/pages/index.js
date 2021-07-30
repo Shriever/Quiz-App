36894674eb3a4e8e94565450d265703773021283
Levi <shrieverdev@gmail.com>
2021-07-30 12:23:58 -0400

    reorganized imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,23 @@
 import React, { useState, useEffect } from "react";
-
+import { Helmet } from "react-helmet";
 import { Container, Grid } from "@material-ui/core";
-import useStyles from "./../utils/styles";
 
+import useStyles from "./../utils/styles";
 import Score from "../components/Score";
 import Answer from "../components/Answer";
 import Question from "../components/Question";
 import Footer from "../components/Footer";
 import displayNextQuestion from "../utils/displayNextQuestion";
-import { Helmet } from "react-helmet";
-
-// LINKS TO APIS FOR THIS PROJECT
-// https://jservice.io/api/random
-// https://api.chucknorris.io/jokes/random
+import initialState from "../utils/initialState";
 
 const IndexPage = () => {
-  const classes = useStyles();
-  const [state, setState] = useState({
-    playerScore: 0,
-    isCorrectAnswer: null,
-    isShowAnswer: false,
-    quote: "",
-    question: {
-      text: "",
-      value: 0,
-    },
-    answer: "",
-  });
+  const [state, setState] = useState(initialState);
 
   useEffect(() => {
     displayNextQuestion(setState);
   }, [setState]);
+
+  const classes = useStyles();
 
   const {
     playerScore,
@@ -54,6 +41,7 @@ const IndexPage = () => {
         <meta charSet='utf-8' />
         <title>Levi's Quiz App</title>
       </Helmet>
+
       <Grid container justifyContent='center'>
         <Score playerScore={playerScore} scoreClass={classes.score} />
 
---
+++ src/utils/initialState.js
@@ -0,0 +1,13 @@
+const initialState = {
+  playerScore: 0,
+  isCorrectAnswer: null,
+  isShowAnswer: false,
+  quote: "",
+  question: {
+    text: "",
+    value: 0,
+  },
+  answer: "",
+};
+
+export default initialState;
